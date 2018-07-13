4ddf45a96cea8b14e72fc4265978c5dda11af12b
John Jardin john.jardin@ukuvuma.co.za <john.jardin@ukuvuma.co.za>
2018-07-13 16:47:57 +0200

    Initial upload of node-red-contrib-uuid @ v0.0.1

---
+++ node-red-contrib-uuid/README.md
@@ -0,0 +1,13 @@
+### Node-RED UUID (Sponsored by [Agilit-e](https://agilite.io))
+ ==========================================
+
+Created by [Agilit-e](https://agilite.io)
+
+* A Node-RED node for simple, fast generation of RFC4122 UUIDS
+* Based on the [uuid](https://www.npmjs.com/package/uuid) NPM Module
+
+ # Author
+ John Jardin (Integration and Cloud Architect at [Agilit-e](https://agilite.io))
+
+ # License
+MIT
---
+++ node-red-contrib-uuid/uuid/uuid.js
@@ -0,0 +1,117 @@
+module.exports = function(RED) {
+  function UUID(config) {
+    RED.nodes.createNode(this,config);
+
+    let node = this;
+    let success = true;
+    let errorMessage = "";
+    let result = "";
+
+    this.on('input', function(msg) {
+      let uuidVersion = "";
+      let namespaceType = "";
+      let namespace = "";
+      let namespaceCustom = "";
+
+      //Check if we need to use programmatic values
+      if(msg.agiliteUtils){
+        if(msg.agiliteUtils.uuidVersion){
+          if(msg.agiliteUtils.uuidVersion !== ""){
+            uuidVersion = msg.agiliteUtils.uuidVersion;
+          }
+        }
+
+        if(msg.agiliteUtils.namespaceType){
+          if(msg.agiliteUtils.namespaceType !== ""){
+            namespaceType = msg.agiliteUtils.namespaceType;
+          }
+        }
+
+        if(msg.agiliteUtils.namespace){
+          if(msg.agiliteUtils.namespace !== ""){
+            namespace = msg.agiliteUtils.namespace;
+          }
+        } 
+        
+        if(msg.agiliteUtils.namespaceCustom){
+          if(msg.agiliteUtils.namespaceCustom !== ""){
+            namespaceCustom = msg.agiliteUtils.namespaceCustom;
+          }
+        }
+      }
+
+      if(uuidVersion === ""){
+        uuidVersion = config.uuidVersion;
+      }
+
+      if(namespaceType === ""){
+        namespaceType = config.namespaceType;
+      }
+
+      if(namespace === ""){
+        namespace = config.namespace;
+      }
+
+      if(namespaceCustom === ""){
+        namespaceCustom = config.namespaceCustom;
+      }
+
+      //Validate Data
+      if(uuidVersion === ""){
+        success = false;
+        errorMessage = "No UUID Version was provided";
+      }else{
+        switch(uuidVersion){
+          case "v3":
+          case "v5":
+            if(namespaceType === ""){
+              success = false;
+              errorMessage = "No Namespace Type was provided";
+            }else if(namespace === ""){
+              success = false;
+              errorMessage = "No Namespace was provided";
+            }else if(namespaceType === "custom" && namespaceCustom === ""){
+                success = false;
+                errorMessage = "No custom Namespace Type was provided";
+              }
+
+            break;
+        }        
+      }
+
+      if(!success){
+        node.error(errorMessage, msg);
+        return false;
+      }
+
+      //Process Function
+      const uuid = require("uuid/" + uuidVersion);
+
+      switch(uuidVersion){
+        case "v1":
+        case "v4":
+          result = uuid();
+          break;
+        case "v3":
+        case "v5":
+          switch(namespaceType){
+            case "dns":
+              result = uuid(namespace, uuid.DNS);
+              break;
+            case "url":
+              result = uuid(namespace, uuid.URL);
+              break;
+            case "custom":
+              result = uuid(namespace, namespaceCustom);
+              break;
+          }
+          break;                  
+      }
+
+      msg.payload = result;
+      node.send(msg);
+    });
+  }
+
+  RED.nodes.registerType("uuid", UUID);
+}
