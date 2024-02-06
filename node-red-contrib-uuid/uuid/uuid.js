8e641dbde89ae0ee973bed6b3c4ed0e94d7705b9
fasblom <filip.asblom@digitaltvilling.se>
2024-02-06 10:58:11 +0100

    Handled consequences of uuid v1,v3 and v5 due to updated package

--- node-red-contrib-uuid/uuid/uuid.js
+++ node-red-contrib-uuid/uuid/uuid.js
@@ -1,129 +1,165 @@
+const uuidv1 = require('uuid/v1');
+const uuidv3 = require('uuid/v3');
+const uuidv4 = require('uuid/v4');
+const uuidv5 = require('uuid/v5');
+
 module.exports = function(RED) {
-  function UUID(config) {
-    RED.nodes.createNode(this,config);
 
-    let node = this;
-    let success = true;
-    let errorMessage = "";
-    let result = "";
-    this.field = config.field || "payload";
-    this.fieldType = config.fieldType || "msg";
+    function UUID(config) {
+        RED.nodes.createNode(this, config);
 
-    this.on('input', function(msg) {
-      let uuidVersion = "";
-      let namespaceType = "";
-      let namespace = "";
-      let namespaceCustom = "";
+        let node = this;
+        let success = true;
+        let errorMessage = "";
+        let result = "";
+        this.field = config.field || "payload";
+        this.fieldType = config.fieldType || "msg";
 
-      //Check if we need to use programmatic values
-      if(msg.agiliteUtils){
-        if(msg.agiliteUtils.uuidVersion){
-          if(msg.agiliteUtils.uuidVersion !== ""){
-            uuidVersion = msg.agiliteUtils.uuidVersion;
-          }
-        }
+        this.on('input', function(msg) {
+          console.log(config)
+            let uuidVersion = "";
+            let namespaceType = "";
+            let namespace = "";
+            let namespaceCustom = "";
 
-        if(msg.agiliteUtils.namespaceType){
-          if(msg.agiliteUtils.namespaceType !== ""){
-            namespaceType = msg.agiliteUtils.namespaceType;
-          }
-        }
+            // Check if we need to use programmatic values
+            if (msg.agiliteUtils) {
+                if (msg.agiliteUtils.uuidVersion) {
+                    if (msg.agiliteUtils.uuidVersion !== "") {
+                        uuidVersion = msg.agiliteUtils.uuidVersion;
+                    }
+                }
 
-        if(msg.agiliteUtils.namespace){
-          if(msg.agiliteUtils.namespace !== ""){
-            namespace = msg.agiliteUtils.namespace;
-          }
-        } 
-        
-        if(msg.agiliteUtils.namespaceCustom){
-          if(msg.agiliteUtils.namespaceCustom !== ""){
-            namespaceCustom = msg.agiliteUtils.namespaceCustom;
-          }
-        }
-      }
+                if (msg.agiliteUtils.namespaceType) {
+                    if (msg.agiliteUtils.namespaceType !== "") {
+                        namespaceType = msg.agiliteUtils.namespaceType;
+                    }
+                }
 
-      if(uuidVersion === ""){
-        uuidVersion = config.uuidVersion;
-      }
+                if (msg.agiliteUtils.namespace) {
+                    if (msg.agiliteUtils.namespace !== "") {
+                        namespace = msg.agiliteUtils.namespace;
+                    }
+                }
 
-      if(namespaceType === ""){
-        namespaceType = config.namespaceType;
-      }
+                if (msg.agiliteUtils.namespaceCustom) {
+                    if (msg.agiliteUtils.namespaceCustom !== "") {
+                        namespaceCustom = msg.agiliteUtils.namespaceCustom;
+                    }
+                }
+            }
 
-      if(namespace === ""){
-        namespace = config.namespace;
-      }
+            if (uuidVersion === "") {
+                uuidVersion = config.uuidVersion;
+            }
 
-      if(namespaceCustom === ""){
-        namespaceCustom = config.namespaceCustom;
-      }
+            if (namespaceType === "") {
+                namespaceType = config.namespaceType;
+            }
 
-      //Validate Data
-      if(uuidVersion === ""){
-        success = false;
-        errorMessage = "No UUID Version was provided";
-      }else{
-        switch(uuidVersion){
-          case "v3":
-          case "v5":
-            if(namespaceType === ""){
-              success = false;
-              errorMessage = "No Namespace Type was provided";
-            }else if(namespace === ""){
-              success = false;
-              errorMessage = "No Namespace was provided";
-            }else if(namespaceType === "custom" && namespaceCustom === ""){
+            if (namespace === "") {
+                namespace = config.namespace;
+            }
+
+            if (namespaceCustom === "") {
+                namespaceCustom = config.namespaceCustom;
+            }
+
+            //Fix to also handle is msg.a
+            if (namespaceCustom === "") {
+              namespaceCustom = config.namespaceCustom;
+            }
+
+            if (namespaceCustom === "") {
+              namespaceCustom = config.namespaceCustom;
+            }
+
+
+            // Validate Data
+            if (uuidVersion === "") {
                 success = false;
-                errorMessage = "No custom Namespace Type was provided";
-              }
+                errorMessage = "No UUID Version was provided";
+            } else {
+                switch (uuidVersion) {
+                    case "v3":
+                    case "v5":
+                        if (namespaceType === "") {
+                            success = false;
+                            errorMessage = "No Namespace Type was provided";
+                        } else if (namespace === "") {
+                            success = false;
+                            errorMessage = "No Namespace was provided";
+                        } else if (namespaceType === "custom" && namespaceCustom === "") {
+                            success = false;
+                            errorMessage = "No custom Namespace Type was provided";
+                        }
 
-            break;
-        }        
-      }
+                        break;
+                }
+            }
 
-      if(!success){
-        node.error(errorMessage, msg);
-        return false;
-      }
+            if (!success) {
+                node.error(errorMessage, msg);
+                return false;
+            }
 
-      //Process Function
-      const uuid = require("uuid/" + uuidVersion);
+            // Process Function
+            try {
+                switch (uuidVersion) {
+                    case "v1":
+                        result = uuidv1();
+                        break;
+                    case "v4":
+                        result = uuidv4();
+                        break;
+                    case "v3":
+                        switch (namespaceType) {
+                            case "dns":
+                                result = uuidv3(namespace, uuidv3.DNS);
+                                break;
+                            case "url":
+                                result = uuidv3(namespace, uuidv3.URL);
+                                break;
+                            case "custom":
+                                // Here you need to ensure namespaceCustom is a valid UUID
+                                result = uuidv3(namespace, namespaceCustom);
+                                break;
+                        }
+                        break;
+                    case "v5":
+                        switch (namespaceType) {
+                            case "dns":
+                                result = uuidv5(namespace, uuidv5.DNS);
+                                break;
+                            case "url":
+                                result = uuidv5(namespace, uuidv5.DNS);
+                                break;
+                            case "custom":
+                                // Here you need to ensure namespaceCustom is a valid UUID
+                                result = uuidv5(namespace, namespaceCustom);
+                                break;
+                        }
+                        break;
+                }
+            } catch (err) {
+                node.error("Error generating UUID: " + err.message, msg);
+                return;
+            }
 
-      switch(uuidVersion){
-        case "v1":
-        case "v4":
-          result = uuid();
-          break;
-        case "v3":
-        case "v5":
-          switch(namespaceType){
-            case "dns":
-              result = uuid(namespace, uuid.DNS);
-              break;
-            case "url":
-              result = uuid(namespace, uuid.URL);
-              break;
-            case "custom":
-              result = uuid(namespace, namespaceCustom);
-              break;
-          }
-          break;                  
-      }
-
-      switch (node.fieldType) {
-        case "msg":
-            RED.util.setMessageProperty(msg, node.field, result);
-            break; 
-        case "flow":
-            node.context().flow.set(node.field, result);
-            break;
-        case "global":
-            node.context().global.set(node.field, result);
-            break;
+            switch (node.fieldType) {
+                case "msg":
+                    RED.util.setMessageProperty(msg, node.field, result);
+                    break;
+                case "flow":
+                    node.context().flow.set(node.field, result);
+                    break;
+                case "global":
+                    node.context().global.set(node.field, result);
+                    break;
+            }
+            node.send(msg);
+        });
     }
-      node.send(msg);
-    });
-  }
 
-  RED.nodes.registerType("uuid", UUID);
-}
+    RED.nodes.registerType("uuid", UUID);
+};
